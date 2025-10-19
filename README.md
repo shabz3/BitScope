5d39f050d44ff042b8be8f0d5c5d920934ffbec2
Shaiban Luqman <shabzie3@gmail.com>
2025-10-19 17:44:25 +0100

    style html with css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 What is the use of app/__init__.py?
+responsive table not used
--- app/main.py
+++ app/main.py
@@ -5,10 +5,14 @@ import uvicorn
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 BASE_URL = os.environ.get("BASE_URL", "https://api.coingecko.com")
+
+# Needed for CSS
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -17,9 +21,14 @@ templates = Jinja2Templates(directory="templates")
 async def get_coingecko_stats(request: Request):
     url = f"{BASE_URL}/api/v3/exchange_rates"
     return_json = requests.get(url).json()
-    print(return_json)
+    all_items = list(return_json["rates"].items())
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"request": request, "items": return_json}
+        request=request,
+        name="index.html",
+        context={
+            "request": request,
+            "items": all_items,
+        },
     )
 
 
