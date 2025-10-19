12b22895165950bcf072cde8e500323de8cc1ac3
Shaiban Luqman <shabzie3@gmail.com>
2025-10-19 20:07:22 +0100

    add 404s

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 import os
 import requests
 import uvicorn
@@ -6,6 +6,7 @@ from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -20,8 +21,11 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def get_coingecko_stats(request: Request):
     url = f"{BASE_URL}/api/v3/exchange_rates"
-    return_json = requests.get(url).json()
-    all_items = list(return_json["rates"].items())
+    try:
+        return_json = requests.get(url).json()
+        all_items = list(return_json["rates"].items())
+    except:
+        raise HTTPException(status_code=500, detail="Failed to fetch")
     return templates.TemplateResponse(
         request=request,
         name="index.html",
@@ -31,6 +35,11 @@ async def get_coingecko_stats(request: Request):
         },
     )
 
+
+# Redirects everything else to 404 page
+@app.exception_handler(404)
+async def custom_404_handler(request, __):
+    return templates.TemplateResponse("404.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
